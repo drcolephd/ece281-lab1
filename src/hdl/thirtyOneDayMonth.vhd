9f72dcb5984a94f27fdd5b73001f973347e86250
drcolephd <156008928+drcolephd@users.noreply.github.com>
2024-02-25 23:34:01 -0700

    Add files via upload

--- src/hdl/thirtyOneDayMonth.vhd
+++ src/hdl/thirtyOneDayMonth.vhd
@@ -11,7 +11,7 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : thirtyOneDayMonth.vhd
---| AUTHOR(S)     : Capt Dan Johnson, ***Your Name Here***
+--| AUTHOR(S)     : Capt Dan Johnson, ***C4C Brandon Sweitzer***
 --| CREATED       : 12/12/2019 Last Modified 06/24/2020
 --| DESCRIPTION   :  This file implements the thirtyOneDaMonth lab.  Using a 4
 --| switch input, the circuit will light up whenever the 4 switches represent a
@@ -56,24 +56,38 @@ library ieee;
 entity thirtyOneDayMonth is 
   port(
 	i_A : in std_logic; -- one of four inputs
-	
-	
-	
+	i_B : in std_logic;
+	i_C : in std_logic;
+	i_D : in std_logic;
 						-- output
+    o_Y : out std_logic
   );
 end thirtyOneDayMonth;
 
 architecture thirtyOneDayMonth_arch of thirtyOneDayMonth is 
 	-- include components declarations and signals
-	
+	signal w_sel0 : std_logic := '0';
+	signal w_sel1 : std_logic := '0';
+	signal w_sel2 : std_logic := '0';
+	signal w_sel3 : std_logic := '0';
 	--signals internal to the architecture are declared and initialized such as w_sel
-  
+    signal w_sel : std_logic_vector (2 downto 0); -- MUX sel
+    
 begin
+
 	-- CONCURRENT STATEMENTS---------------------------------------
 	--assigning names to reflect original schematics (for ease of understanding if you wish to)
-	w_sel(0) <= i_C;	-- one
-	--finish assigning signals
+	w_sel0 <= i_A;	-- one
 	
-	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.	
+	--finish assigning signals
+	w_sel1 <= i_B;
+    w_sel2 <= i_C;
+    w_sel3 <= i_D;
+	--enter your logic here to implement the mux.  See VHDL reference sheet for MUX syntax.
+	o_Y <= w_sel0 when (w_sel = "00") else
+	       w_sel1 when (w_sel = "01") else
+	       w_sel2 when (w_sel = "10") else
+	       w_sel3 when (w_sel = "11") else
+	       '0';
 	---------------------------------------------------------------	
 end thirtyOneDayMonth_arch;
